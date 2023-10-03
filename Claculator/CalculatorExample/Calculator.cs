894da27305aa42cc10174261a89b16d08fd252c0
Slavena Slavova <106436147+SlavenaS@users.noreply.github.com>
2023-10-03 14:06:41 +0300

    Add files via upload

---
+++ Claculator/CalculatorExample/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalculatorExample
+{
+    public class Calculator
+    {
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtact(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Devide(double x, double y)
+        {
+            //delene na 0
+            return x / y;
+        }
+        public long Factoriel(int x)
+        {
+            long rezul = 1;
+            for (int i = 2; i <= x; i++)
+            {
+                rezul *= i;
+            }
+           return rezul;
+        }
+        public double Stepen(double baseNumber, int power)
+        {
+            //stepenuvane s -chisla
+            double rezul = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                rezul *=baseNumber;
+            }
+            return rezul;
+        }
+    }
+}
---
+++ Claculator/CalculatorExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi chislo 1 ");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.Write("Vuvedi chislo 2 ");
+            double number2 = double.Parse(Console.ReadLine());
+            double result = 0;
+            Calculator elka = new Calculator();
+
+            result = elka.Sum(number1, number2);
+            Console.WriteLine($"{number1}+{number2}= "+result);
+
+            result = elka.Subtact(number1, number2);
+            Console.WriteLine($"{number1}-{number2}= "+ result);
+
+            result = elka.Multiply(number1, number2);
+            Console.WriteLine($"{number1}*{number2}= " + result);
+
+            result = elka.Devide(number1, number2);
+            Console.WriteLine($"{number1}:{number2}= " + result);
+
+            result = elka.Factoriel((int)number1);
+            Console.WriteLine($"{number1}!= " + result);
+
+            result = elka.Stepen(number1,(int)number2);
+            Console.WriteLine($"{number1} ^ {(int)number2 }=" + result);
+        }
+    }
+}
